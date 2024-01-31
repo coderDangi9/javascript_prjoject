78192c5df3bfa517178a65870f2cc77cb69ff433
Sandip <sandipdangi682@gmail.com>
2024-01-31 08:43:50 -0800

    d1

--- day-1/app.js
+++ day-1/app.js
@@ -1,4 +1,18 @@
+// Javascript program to print Helloworld
 const variable = "Hello guys";
 console.log(variable);
 alert(variable);
 document.write(variable);
+
+//javascript program to Add two number
+
+// let num1 =9;// variable and constant declare
+// let num2 = 4;
+
+//userdefine variable;
+let num1 = parseInt(prompt("Enter first number :"));
+let num2 = parseInt(prompt("Enter second number :"));
+
+const Sum = num1+num2; 
+
+console.log(`Total sum of ${num1} and ${num2} is ${Sum}`);
