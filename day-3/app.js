0efb5425bd288acfb82eeb5e925acf58bff1a185
Sandip <sandipdangi682@gmail.com>
2024-02-12 04:50:22 -0800

    d

--- day-3/app.js
+++ day-3/app.js
@@ -33,7 +33,7 @@ for (let j = 3; j < n; j++) {
 console.log(isPrime);
 */
 //javascript program to find the factorial number.
-
+/*
 let num = parseInt(prompt("Enter the number which you want to factorial."));
 let result = 1;
 if (num == 0) {
@@ -43,4 +43,73 @@ if (num == 0) {
     result *= i;
   }
   console.log(`${result}`);
+}
+*/
+
+// To show the multiplication table
+/*
+let n = parseInt(
+  prompt("Enter the value which you want multiplication table.")
+);
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${n} x ${i} = ${n * i}`);
+  // document.write(` \n ${n} x ${i} = ${n * i}\n`);
+}
+
+*/
+
+// To find the Fibonacci sequence :: 0,1,1,2,3....
+/*
+console.log("sandip");
+const n = parseInt(prompt("Enter the nth number."));
+let n1 = 0,
+  n2 = 1,
+  n3;
+
+for (let i = 1; i < n; i++) {
+  console.log(n1);
+  n3 = n1 + n2;
+  n1 = n2;
+  n2 = n3;
+}
+*/
+
+// Javascript program to print all Promise.
+/*
+const lowerNum = parseInt(prompt(`Enter lower Number.`));
+const higherNum = parseInt(prompt(`Enter Higher number.`));
+console.log(`The prime number betn ${lowerNum} and ${higherNum}`);
+
+for (i = lowerNum; i < higherNum; i++) {
+  let flag = 0;
+  for (j = 2; j < i; j++) {
+    if (i % j == 0) {
+      flag = 1;
+      break;
+    }
+  }
+  // The number greater than 1 and not divisible by other numbers.
+  if (i > 1 && flag == 0) {
+    console.log(i);
+  }
+}
+*/
+
+// To check the Armstrong number
+let num = prompt("Enter the any number.");
+let n = num.length;
+let sum = 0;
+let temp = num;
+while (temp > 0) {
+  let rem = temp % 10;
+  sum += rem ** n;
+  temp = parseInt(temp / 10); // convert float into integer.
+}
+
+// console.log(sum);
+if (sum == num) {
+  console.log(`${num} is armstrong number.`);
+} else {
+  console.log(`${num} is not armstrong number.`);
 }
