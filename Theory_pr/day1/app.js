c496e7096f95ac6907508c5c78bb3488ed82efa9
Sandip <sandipdangi682@gmail.com>
2024-03-11 05:12:14 -0700

    d

---
+++ Theory_pr/day1/app.js
@@ -0,0 +1,35 @@
+// let a ;
+// console.log(a);
+// const b=9;
+// console.log(b);
+
+// let price = 300;
+// console.log(typeof price);
+
+// const student ={
+//     name : "Sandip",
+//     age :30
+// };
+// console.log(typeof student.age);
+// student["name"] ="Spider";
+// console.log(student.name);
+
+//Let's Practice
+
+// Create a const object called "Product" to store information shown in the picture.
+/*/
+    const Product={
+        name : ballPen,
+        Price:100,
+        rating :4,
+        discount :10% off 
+    };
+
+*/
+// Create a const object called "profile " to store information show in the picture.
+const Profile = {
+  name: "ShradhaKhapra",
+  Post: 195,
+  followers: 596000,
+  isFollow: true,
+};
