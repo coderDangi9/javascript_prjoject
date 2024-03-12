476c7081cffee79a40537989bb1b1b860b20e597
Sandip <sandipdangi682@gmail.com>
2024-03-12 04:54:46 -0700

    d

---
+++ Theory_pr/day2/app.js
@@ -0,0 +1,31 @@
+//Arithmetic Operation
+/*
+let a = 8;
+let b = 2;
+
+console.log("a+b =" + a + b);
+console.log(`a-b =${a - b}`);
+console.log(`a*b =${a * b}`);
+console.log(`a/b =${a / b}`);
+console.log(`a%b =${a % b}`);
+console.log(`a**b =${a ** b}`);
+console.log(`++a =${++a}`);
+*/
+
+// Comprasion Operators
+/*
+let a = 5; // Number
+let b = "5"; //String
+console.log(`a==b :- ${a == b}`);
+console.log(`a===b :- ${a === b}`);
+console.log(`a>=b :- ${a >= b}`);
+*/
+
+//Logical operators
+let a = 8;
+let b = 3;
+let cond1 = a < b;
+let cond2 = a === 8;
+console.log(`cond1 && cond2 :- ${cond1 && cond2}`);
+console.log(`cond1 || cond2 :- ${cond1 || cond2}`);
+console.log(`! cond1 :- ${!cond1}`);
