3d3270942ec97addf8240e03bd6f66cf7d757207
Sandip <sandipdangi682@gmail.com>
2024-03-25 04:18:40 -0700

    d

---
+++ Theory_pr/day3/app.js
@@ -0,0 +1,59 @@
+// console.log("Sandip");
+//Calculate the Sum of first nth number.
+/*let sum = 0,
+  n = 9;
+for (let i = 1; i <= n; i++) {
+  sum += i;
+}
+console.log(sum);
+*/
+
+/*let i = 1;
+while (i < 10) {
+  console.log("Happy holi");
+}
+i++;
+
+
+let i = 20;
+do {
+  console.log("sandip");
+  i++;
+} while (i <= 10);
+
+let size = 0;
+let string = "SpiderKing";
+for (let i of string) {
+  size++;
+  console.log("i", i);
+}
+console.log("Size:-", size);
+
+
+let student = {
+  name: "Sandip",
+  age: 20,
+  address: "Ghorahi",
+  result: "pass",
+};
+for (let i in student) {
+  console.log(i, ": ", student[i]);
+}
+
+
+// Print all even numbers from 0 to 100.
+for (i = 0; i <= 100; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
+*/
+// Create a game where you start with any random game number.
+// Ask the user to keep guessing the game number until the user
+// enters correct value.
+let gnum = 34;
+let user = prompt("Please, Enter your guess number.");
+while (user != gnum) {
+  user = prompt("Please, Enter your guess number.");
+}
+console.log(`Congrats,You Won the Game.`);
