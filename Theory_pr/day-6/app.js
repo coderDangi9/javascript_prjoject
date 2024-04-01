fdee92b796b8bbdeefe8c3b41f55deae56e8d510
Sandip <sandipdangi682@gmail.com>
2024-04-01 14:43:02 -0700

    d

--- Theory_pr/day-6/app.js
+++ Theory_pr/day-6/app.js
@@ -1,1 +1,12 @@
-document.body.childNodes[2].innerText = "abxcd";
+// // document.body.childNodes[3].innerText = "abxcd";
+// let headi = document.getElementById("myId");
+// console.dir(headi);
+// let head = document.getElementsByClassName("myClass");
+// console.dir(head);
+// let par = document.getElementsByTagName("p");
+// console.dir(par);
+
+let firstEle = document.querySelector("#myId");
+console.dir(firstEle);
+let allEle = document.querySelectorAll(".myClass");
+console.dir(allEle);
