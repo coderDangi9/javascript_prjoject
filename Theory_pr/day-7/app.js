fa2a3ce9a0c9039fa36525f2b9f406fd9a5072a1
Sandip <sandipdangi682@gmail.com>
2024-04-09 05:24:36 -0700

    d

--- Theory_pr/day-7/app.js
+++ Theory_pr/day-7/app.js
@@ -27,7 +27,36 @@
 // div.before(newBtn);
 // p.after(newBtn);
 
-let newHeading = document.createElement("h1");
-newHeading.innerHTML = "<i>Hi, I am new !</i>";
+// let newHeading = document.createElement("h1");
+// newHeading.innerHTML = "<i>Hi, I am new !</i>";
 
-document.querySelector("body").prepend(newHeading);
+// document.querySelector("body").prepend(newHeading);
+
+// let par = document.querySelector("p");
+// par.remove();
+
+// newHeading.remove();
+
+// const del = document.body.appendChild(document.createElement("h2"));
+
+/*
+QS. Create a new button element.Give it a text "click me", background
+color of red and text color of white.
+Insert the button as the first element inside the body tag.
+
+*/
+let but = document.createElement("button");
+but.innerHTML = "Click me";
+
+but.style.backgroundColor = "red";
+but.style.color = "white";
+
+document.querySelector("body").prepend(but);
+
+/*
+Create a <p> tag in HTML, give it a class & some styling.
+Now create a new class in CSS and try to append this class to the
+<p> else Did you notice, how you overwrite the class name when you 
+add a newthing . solve this problem using classlist.
+*/
+let par = document.querySelector("p");
